d8406c41d4750625a651948ef3d1d1bbfe7cf637
CHRIS GAUGE <laptop@CHRISs-MacBook-Pro.local>
2016-02-09 07:03:48 -0500

    NLC Example

---
+++ Raspberry-Pi-Personal-Assistant-with-IBM-Watson/bluemix.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#Enter the IBM Watson Bluemix credentials and the URL of the Natural Language Classifier service you abtained
+#from https://www.ibm.com/smarterplanet/us/en/ibmwatson/developercloud/services-catalog.html
+
+Bluemix_CRED = "username:password"
+url = 'https://gateway.watsonplatform.net/natural-language-classifier/api/v1/classifiers'
+
+user = Bluemix_CRED.split(':')[0]
+pwd = Bluemix_CRED.split(':')[1]
+
